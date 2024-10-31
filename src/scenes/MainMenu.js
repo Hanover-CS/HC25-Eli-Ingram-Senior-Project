e4d9117529829d94913d16b389a10bc99cbc5494
Eli Ingram <ingrame25@hanover.edu>
2024-10-31 13:50:43 -0400

    Adjusted Menu background size

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -9,11 +9,11 @@ export class MainMenu extends Scene
 
     create ()
     {
-        this.add.image(512, 384, 'background').setDepth(0);
+        this.add.image(512, 384, 'background').setSizeToFrame(true);
 
-        let playButton = this.add.image(512, 275, 'playButton').setDepth(1);
+        let playButton = this.add.image(512, 275, 'playButton');
 
-        let optionsButton = this.add.image(512, 375, 'optionsButton').setDepth(1);
+        let optionsButton = this.add.image(512, 375, 'optionsButton');
 
         playButton.setInteractive();
         playButton.once('pointerdown', () => {
